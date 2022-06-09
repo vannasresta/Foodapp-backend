ef598b34c284358c93ae118afd075600bd41284f
vanna sresta <srestareddyvanna@gmail.com>
2022-06-09 13:20:15 +0530

    Foodapp-backend

---
+++ src/main/java/ecommerce/EcommerceBackendApplication.java
@@ -0,0 +1,15 @@
+package ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
